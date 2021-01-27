3152a26a05e5de055fe9f75c053613296cc5d886
harimbu <harimbu@gmail.com>
2021-01-27 20:44:48 +0900

    insta

--- src/main.js
+++ src/main.js
@@ -4,15 +4,10 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
-import { createApi } from 'unsplash-js'
+import '@/services/unsplash'
 
 Vue.config.productionTip = false
 
-const api = new createApi({
-  accessKey: 'e6MD8Gpf5agAWPP4BsapuMe7Htj7sOSXMluIMTbuAXo'
-})
-
-Vue.prototype.$unsplash = api
 Vue.prototype.$axios = axios
 Vue.prototype.$eventBus = new Vue()
 
